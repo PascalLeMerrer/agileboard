8aae65a57d138bb52226b4dc0a29d5d5a7e91870
Pascal Le Merrer <pascal.lemerrer@orange.com>
2013-06-03 00:21:00 +0200

    Added angular JS file serving

--- AgileBoard/settings.py
+++ AgileBoard/settings.py
@@ -1,4 +1,5 @@
 # Django settings for AgileBoard project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +62,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "board/static/")
+#'/Users/pascallemerrer/Dev/AgileBoard/board/static/'
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
